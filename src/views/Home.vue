2d036a39dbb70e92addaa27b50b0ede4f01bc304
badtime1407 <badtime1407@gmail.com>
2026-01-19 21:12:08 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-pink-100">
-    <h1 class="text-4xl font-bold text-pink-600">
-      homepage
-    </h1>
+    
   </div>
 </template>
