d96dbcd6e06a4bee30698d51216dec2c48fa6935
badtime1407 <badtime1407@gmail.com>
2026-01-19 15:53:32 +0700

    Start-project

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-pink-100">
+    <h1 class="text-4xl font-bold text-pink-600">
+      homepage
+    </h1>
+  </div>
+</template>
