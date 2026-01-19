1e8e45bd3da33b77dc603dedeb8271c74ff37be3
badtime1407 <badtime1407@gmail.com>
2026-01-19 16:24:47 +0700

    add Packages

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
+import Packages from "../views/Packages.vue";
 
 
 const routes = [
   { path: "/", component: Home },
   { path: "/login", component: Login },
+  { path: "/packages", component: Packages },
+
 ];
 
 const router = createRouter({
---
+++ src/views/Packages.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>dijfskdg</h1>
+    </div>
+</template>
